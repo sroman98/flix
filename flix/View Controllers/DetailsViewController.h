28d1628f7372a3e7f1a0fb8f5b03f9bd95d33851
Sandra Roman Rivera <sroman98@fb.com>
2019-06-27 12:28:48 -0700

    add basic UIActivity indicator

---
+++ flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  flix
+//
+//  Created by sroman98 on 6/26/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flix/Views/MovieCell.h
@@ -0,0 +1,21 @@
+//
+//  MovieCell.h
+//  flix
+//
+//  Created by sroman98 on 6/26/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
