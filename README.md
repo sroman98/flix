9b372d29668e3fe30203eca5e9f25d3e661d2e74
Sandra Roman Rivera <sroman98@fb.com>
2019-06-28 12:02:21 -0700

    add detail view when tapping collection view poster

--- README.md
+++ README.md
@@ -17,13 +17,13 @@ The following **optional** features are implemented:
 
 - [x] User sees an error message when there's a networking error.
 - [x] Movies are displayed using a CollectionView instead of a TableView.
-- [ ] User can search for a movie.
+- [ ] User can search for a movie. *
 - [ ] All images fade in as they are loading.
-- [ ] User can view the large movie poster by tapping on a cell.
+- [ ] User can view the large movie poster by tapping on a cell. *
 - [ ] For the large poster, load the low resolution image first and then switch to the high resolution image when complete.
 - [ ] Customize the selection effect of the cell.
-- [ ] Customize the navigation bar.
-- [ ] Customize the UI.
+- [ ] Customize the navigation bar. *
+- [ ] Customize the UI. *
 
 The following **additional** features are implemented:
 
--- flix/View Controllers/MoviesGridViewController.m
+++ flix/View Controllers/MoviesGridViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MoviesGridViewController.h"
+#import "DetailsViewController.h"
 #import "MovieCollectionCell.h"
 #import "UIImageView+AFNetworking.h"
 
@@ -64,16 +65,6 @@
     [task resume];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
     
@@ -92,6 +83,20 @@
 
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return self.movies.count;
+}
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    UICollectionViewCell *tappedCell = sender;
+    NSIndexPath *indexPath =  [self.collectionView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    detailsViewController.movie = movie;
 }
 
 @end
